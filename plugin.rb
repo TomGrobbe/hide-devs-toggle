cfffc678d8a755157cbedaff4a0d357819e56eac
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2018-02-13 20:54:30 +0100

    another possible fix.

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: hide_devs
 # about: Hide pizza users, discourse plugin.
-# version: 0.0.3
+# version: 0.0.4
 # authors: Tom Grobbe
 # url: https://github.com/TomGrobbe/hide-devs-toggle
 
@@ -47,7 +47,7 @@ after_initialize do
 				topic_id: post.topic_id,
 				new_owner: pizzaGroup.users.sample,
 				acting_user: pizzaGroup.users.sample,
-				skip_revision: false ).change_owner! unless SiteSetting.hide_devs_enabled == false
+				skip_revision: false ).change_owner! unless SiteSetting.hide_devs_hide_enabled == false
 		# end
 	end
 end
