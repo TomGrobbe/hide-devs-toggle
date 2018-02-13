710aa1925d9fb512bf8d820d88a63a8b6498d236
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2018-02-13 22:21:06 +0100

    another test

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: hide_devs
 # about: Hide pizza users, discourse plugin.
-# version: 0.0.471
+# version: 0.0.472
 # authors: Tom Grobbe
 # url: https://github.com/TomGrobbe/hide-devs-toggle
 
@@ -41,17 +41,18 @@ after_initialize do
 	end
 
 	DiscourseEvent.on(:post_created) do |post, opts, user|
-		next unless user.group_ids.include? hide.id
+		#next unless user.group_ids.include? hide.id
+		next if user.group_ids.include? hide.id
 		# next unless post.content.include? "<show>"
 		# puts post
-		puts "Hey console here I am"
+		#puts "Hey console here I am"
 		# puts post.raw
-		logger.info "blah"
-        next unless post.raw.include? "show"
+		#logger.info "blah"
+		#next unless post.raw.include? "show"
 		PostOwnerChanger.new( post_ids: [post.id],
 				topic_id: post.topic_id,
 				new_owner: pizzaGroup.users.sample,
 				acting_user: pizzaGroup.users.sample,
-				skip_revision: false ).change_owner! if post.raw.include? "show"
+				skip_revision: false ).change_owner!
 	end
 end
