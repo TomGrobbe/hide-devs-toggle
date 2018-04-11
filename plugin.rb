af15f31622051d7b296bb3b914ec81845305663a
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2018-04-12 01:37:56 +0200

    made it fivem forums compatible

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,8 @@
-# name: hide_devs
+# name: discourse-hide-devs
 # about: Hide developers discourse plugin.
 # version: 2.0
-# authors: CitizenFX Collective & Vespura
-# url: https://github.com/TomGrobbe/hide-devs-toggle
+# authors: CitizenFX Collective & Tom Grobbe
+# url: https://github.com/blattersturm/discourse-hide-devs
 
 enabled_site_setting :hide_devs_enabled
 
@@ -16,8 +16,8 @@ after_initialize do
 	::Post.register_custom_field_type('hide_devs', :boolean)
 	register_post_custom_field_type('hide_devs', :boolean)
 	add_permitted_post_create_param('hide_devs')
-	hide = Group.find_by name: 'hide_devs'
-	pizzaGroup = Group.find_by name: 'Pizza'
+	hide = Group.find_by name: 'hide_developers'
+	elementsGroup = Group.find_by name: 'elements'
 
 	module ::HideDevs; end
 
@@ -29,13 +29,13 @@ after_initialize do
 
 	module ::HideDevs::PostCreatorExtensions
 		def initialize(user, opts)
-			hide = Group.find_by name: 'hide_devs'
-			pizzaGroup = Group.find_by name: 'Pizza'
+			hide = Group.find_by name: 'hide_developers'
+			elementsGroup = Group.find_by name: 'elements'
 
 			super
 
 			if ((user.group_ids.include? hide.id) && (opts[:hide_devs].to_s == "true"))
-				@user = pizzaGroup.users.sample
+				@user = elementsGroup.users.sample
 			else
 				@user = user
 			end
@@ -55,8 +55,8 @@ after_initialize do
 		next unless ((user.group_ids.include? hide.id) && (opts[:hide_devs].to_s == "true"))
 		PostOwnerChanger.new(post_ids: [post.id],
 				topic_id: post.topic_id,
-				new_owner: pizzaGroup.users.sample,
-				acting_user: pizzaGroup.users.sample,
+				new_owner: elementsGroup.users.sample,
+				acting_user: elementsGroup.users.sample,
 				skip_revision: false).change_owner!
 	end
 end
