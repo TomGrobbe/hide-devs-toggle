32ac874c3d94e328e42ad1e8e1dc4e92ba0e52c8
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2018-04-11 22:58:08 +0200

    more logging

--- plugin.rb
+++ plugin.rb
@@ -56,6 +56,7 @@ after_initialize do
 	DiscourseEvent.on(:post_created) do |post, opts, user|
 		#		next unless ((user.group_ids.include? hide.id) && !(opts[:raw].to_s.include? "<NoHideDevs>"))
 		puts(opts[:hide_devs].to_s)
+		puts((opts[:hide_devs].to_s == "true"))
 		next unless ((user.group_ids.include? hide.id) && (opts[:hide_devs].to_s == "true"))
 		PostOwnerChanger.new(post_ids: [post.id],
 				topic_id: post.topic_id,
@@ -64,3 +65,55 @@ after_initialize do
 				skip_revision: false).change_owner!
 	end
 end
+
+
+
+#enabled_site_setting :hide_devs_enabled
+#
+#require_dependency 'post_creator'
+#require_dependency 'topic_creator'
+#
+#after_initialize do
+#	hide = Group.find_by name: 'hide'
+#	pizzaGroup = Group.find_by name: 'Pizza'
+#
+#	module ::HideDevs; end
+#
+#	module ::HideDevs::WebHookTopicViewSerializerExtensions
+#		def include_post_stream?
+#			true
+#		end
+#	end
+#
+#	module ::HideDevs::PostCreatorExtensions
+#		def initialize(user, opts)
+#			hide = Group.find_by name: 'hide'
+#			pizzaGroup = Group.find_by name: 'Pizza'
+#
+#			super
+#
+#			if ((user.group_ids.include? hide.id) && !(opts[:raw].to_s.include? "<show>"))
+#				@user = pizzaGroup.users.sample
+#			else
+#				@user = user
+#			end
+#		end
+#	end
+#
+#	class ::PostCreator
+#		prepend ::HideDevs::PostCreatorExtensions
+#	end
+#
+#	class ::WebHookTopicViewSerializer
+#		prepend ::HideDevs::WebHookTopicViewSerializerExtensions
+#	end
+#
+#	DiscourseEvent.on(:post_created) do |post, opts, user|
+#		next unless ((user.group_ids.include? hide.id) && !(opts[:raw].to_s.include? "<show>"))
+#		PostOwnerChanger.new( post_ids: [post.id],
+#			topic_id: post.topic_id,
+#			new_owner: pizzaGroup.users.sample,
+#			acting_user: pizzaGroup.users.sample,
+#			skip_revision: false ).change_owner!
+#	end
+#end
